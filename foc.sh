fd3f421f057da0f52a536d4ac93581d2bcb223d9
Josef Ladra <josef.ladra@gmail.com>
2021-01-09 08:03:20 +0100

    Add files via upload

---
+++ foc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+then
+	echo "Awaiting parametr of resolution (0 or 1)" 1>&2
+	exit 1
+fi
+
+zoom=$1
+
+echo "setting zoom" $zoom
+echo "Running for 60s"
+
+if [ $zoom -eq 0 ]
+then
+	python3 Video_server.py | python3 Camera_stream.py	
+else 
+	python3 Video_server.py | python3 Camera_stream_zoom.py
+fi
+	
